2e621ab70f14e58b9d5229d1c1faf03529d474ff
Alejandro Rodríguez <alerodriargui@gmail.com>
2024-09-26 17:03:32 +0200

    update

--- go.sh
+++ go.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
 #compile
-java -jar lib/jflex-full-1.8.2.jar grammar/lcalc.flex  
-java -cp .:lib/java-cup-11b.jar java_cup.Main < grammar/ycalc.cup
+java -jar lib/jflex-full-1.8.2.jar lcalc.flex  
+java -cp .:lib/java-cup-11b.jar java_cup.Main < ycalc.cup
 javac -cp .:lib/java-cup-11b.jar Main.java
 
 #run
