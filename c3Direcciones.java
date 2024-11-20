91de455bdfe998e0f6070850278242d924dbf49c
sgarat <sergiogaratestelrich@gmail.com>
2024-11-20 22:55:03 +0100

    subo pintado de instrucciones 3d

--- c3Direcciones.java
+++ c3Direcciones.java
@@ -1,9 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class c3Direcciones {
 
-    private ArrayList<instruction3Address> instructions = new ArrayList<>();
-    private int tempCounter = 0; // Contador para variables temporales
+    private static String PATH = "output\\c3d.txt";
+    private ArrayList<instruction3Address> instructions;
+    private int tempCounter; // Contador para variables temporales
+
+    public c3Direcciones() {
+        instructions = new ArrayList<>();
+        tempCounter = 0;
+    }
 
     // Método para generar un nuevo nombre de variable temporal
     public String newTemp() {
@@ -17,9 +27,25 @@ public class c3Direcciones {
 
     // Método para imprimir todas las instrucciones
     public void printInstructions() {
-        for (instruction3Address instr : instructions) {
-            System.out.println(instr);
+        String result = "-----------------------------------------------\n"
+                + "---------------- C3@ code list"
+                + " ----------------\n"
+                + "-----------------------------------------------\n";
+        for (int i = 0; i < instructions.size(); i++) {
+            result += instructions.get(i) + "\n";
         }
-
+        try {
+            // File Writter
+            File file;
+            file = new File(PATH);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(result);
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println("ERROR WRITING C3@");
+        }
     }
 }
